098826effdd9fd0cf094d914405b5fc966c4c7db
Simren Singh <simren.singh@aproda.ch>
2024-03-09 18:52:04 +0100

    initvalues for time calculation

--- src/src.ino
+++ src/src.ino
@@ -1,62 +1,109 @@
 char InputChar;
 bool loggedActivity;
-bool overflow;  //end of Stream
+bool overflow; // end of Stream
 bool movementActive;
 bool currentmovementActive;
 byte Streamlenght;
 String Command;
-byte MovementSensor = 53;
+const byte MovementSensorPin = 53;
+unsigned int MovementActivityTime = 5000;
 unsigned long timeTill;
+unsigned long counter;
+unsigned long timeCalculation;
+byte day;
+byte month;
+byte year;
+byte hour;
+byte minute;
+byte second;
+byte BaseDay;
+byte Basemonth;
+byte Baseyear;
+byte Basehour;
+byte Baseminute;
+byte monthdays[12];
 
-struct LogEntry {
+struct LogEntry
+{
   byte day;
   byte month;
   byte year;
   byte hour;
   byte minute;
   byte second;
-  LogEntry* next;  // linked list pointer
+  LogEntry *next; // linked list pointer
 };
 
+LogEntry *logList = NULL; // Head of the linked list
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
   loggedActivity = false;
   Streamlenght = 0;
   overflow = false;
   movementActive = false;
-  pinMode(MovementSensor, INPUT);
+  pinMode(MovementSensorPin, INPUT);
+  // default values
+  monthdays[0] = 31; // jan
+  monthdays[1] = 29;
+  monthdays[2] = 31;
+  monthdays[3] = 30; // april
+  monthdays[4] = 31;
+  monthdays[5] = 30;
+  monthdays[6] = 31; // jul
+  monthdays[7] = 31;
+  monthdays[8] = 30;
+  monthdays[9] = 31; // okt
+  monthdays[10] = 30;
+  monthdays[11] = 31;
+  BaseDay = 9;
+  Basemonth = 3;
+  Baseyear = 24;
+  Basehour = 18;
+  Baseminute = 30;
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  if (!loggedActivity) {
+  if (!loggedActivity)
+  {
     Serial.print(F("Waiting"));
     Serial.println();
     loggedActivity = true;
   }
 
-  currentmovementActive = digitalRead(MovementSensor) == HIGH;
-  if (movementActive || currentmovementActive) {//movement sensor activity
-    if ((!movementActive) && currentmovementActive) {//first contact
+  currentmovementActive = digitalRead(MovementSensorPin) == HIGH;
+  if (movementActive || currentmovementActive)
+  { // movement sensor activity
+    if ((!movementActive) && currentmovementActive)
+    { // first contact
       Serial.println();
       Serial.print("Movement detected");
-      timeTill = millis() + 5000;
+      timeTill = millis() + MovementActivityTime;
       movementActive = true;
-    } else if (movementActive && currentmovementActive) {//recurring contact (first contact not finished yet)
-      timeTill = millis() + 5000;
-    } else if (movementActive && (!currentmovementActive)) {//check if contact finished
+      counter = 0;
+    }
+    else if (movementActive && currentmovementActive)
+    { // recurring contact (first contact not finished yet)
+      timeTill = millis() + MovementActivityTime;
+      Serial.println("debug: still movement " + String(counter));
+      counter += 1;
+    }
+    else if (movementActive && (!currentmovementActive))
+    { // check if contact finished
+      Serial.println("debug: no movement " + String(counter));
+      counter += 1;
       movementActive = (timeTill < millis());
-      if (!movementActive) {//contact finished
+      if (!movementActive)
+      { // contact finished
         Serial.println();
         Serial.println("Movement stopped");
       }
     }
   }
-
-
-
 
   //   while(digitalRead(MovementSensor) == HIGH)
   //   {
@@ -66,25 +113,33 @@ void loop() {
   //   Serial.println("Movement stopped");
   // }
 
-  while (Serial.available() != 0) {
+  while (Serial.available() != 0)
+  {
     Streamlenght += 1;
     InputChar = Serial.read();
     delay(100);
 
-    if (overflow) {
+    if (overflow)
+    {
       Serial.print(InputChar);
-    } else {
+    }
+    else
+    {
       overflow = (Streamlenght > 4);
-      if (!overflow) {
+      if (!overflow)
+      {
         Command += InputChar;
-      } else {
+      }
+      else
+      {
         Serial.print(F("Input above 4:"));
         Serial.print(InputChar);
       }
     }
   }
 
-  if (Streamlenght != 0) {
+  if (Streamlenght != 0)
+  {
     Streamlenght = 0;
     loggedActivity = false;
     overflow = false;
@@ -97,7 +152,24 @@ void loop() {
   }
 }
 
-void Interpret() {
+void CreateTimeStamp()
+{
+  LogEntry *newEntry = new LogEntry;
+
+  newEntry.second = 30;
+  newEntry->second = 30;
+    timeCalculation = millis();
+  newEntry->day = baseday;
+  newEntry->month = Basemonth;
+  newEntry->year = baseyear;
+  newEntry->hour = basehour;
+  newEntry->minute = baseminute;
+  //newEntry->second = now.second();
+  newEntry->next = NULL;
+}
+
+void Interpret()
+{
   // switch (Command) {
   // case "HELP":
   //   Serial.print("?,help = get Commands");
