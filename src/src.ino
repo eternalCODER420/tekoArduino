ed322ad37ad956758dce997eeffcbfc12fd74de2
Simren Singh <simren.singh@aproda.ch>
2024-03-11 15:09:04 +0100

    export logger to own module

--- src/src.ino
+++ src/src.ino
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include "SerialCommands.h"
 #include "MemoryManagment.h"
+#include "Logger.h"
 
 // Author: simrem singh
 
@@ -11,22 +12,6 @@ bool movementActive;
 bool currentMovementActive;
 
 unsigned long timeTill;
-
-byte monthDays[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; // Days in each month
-
-struct LogEntry
-{
-  byte day;
-  byte month;
-  // byte year;
-  byte hour;
-  byte minute;
-  byte second;
-  LogEntry *next; // linked list pointer
-};
-
-LogEntry *logListStack = NULL; // Head of the linked list
-LogEntry baseValues;
 
 void setup()
 {
@@ -62,7 +47,7 @@ void checkMovement()
     { // recurring contact (first contact not finished yet)
       timeTill = millis() + MovementActivityTime;
     }
-    else if (movementActive && (!currentMovementActive))
+    else// if (movementActive && (!currentMovementActive))
     { // check if contact finished
       movementActive = (timeTill < millis());
       if (!movementActive)
@@ -129,168 +114,3 @@ void interpret(String UserCommand)
   Serial.print(F("input not valid. Write help to get valid inputs"));
 }
 
-
-void initLogger()
-{
-  baseValues.day = 10;
-  baseValues.month = 3;
-  baseValues.hour = 15;
-  baseValues.minute = 5;
-}
-
-void PrintLog()
-{
-  Serial.println(F("Logged Entries:"));
-  LogEntry *currentEntry = logListStack;
-  while (currentEntry != NULL)
-  {
-    PrintLogEntry(currentEntry);
-    currentEntry = currentEntry->next;
-  }
-  Serial.println();
-}
-
-void PrintLogEntry(LogEntry *printEntry)
-{
-  Serial.println();
-  Serial.print(printEntry->day);
-  Serial.print(F("/"));
-  Serial.print(printEntry->month);
-  Serial.print(F("/-"));
-  Serial.print(printEntry->hour);
-  Serial.print(F(":"));
-  Serial.print(printEntry->minute);
-  Serial.print(F(":"));
-  Serial.println(printEntry->second);
-}
-
-void createTimeStamp()
-{
-  if (memoryLimitReached)
-    return;
-
-  byte tempMonth;
-  byte tempDaysInMonth;
-  unsigned long timeCalculation;
-
-  LogEntry *newEntry = new LogEntry;
-  timeCalculation = millis() / 1000; // millisends -> seconds
-  newEntry->second = timeCalculation % 60;
-  timeCalculation /= 60; // get seconds
-  timeCalculation += baseValues.minute;
-  newEntry->minute = timeCalculation % 60;
-  timeCalculation /= 60; // get minutes
-  timeCalculation += baseValues.hour;
-  newEntry->hour = timeCalculation % 24;
-  timeCalculation /= 24; // get days
-  timeCalculation += baseValues.day;
-  tempMonth = baseValues.month; // currentmonth
-  tempDaysInMonth = monthDays[tempMonth - 1];
-  while (tempDaysInMonth < timeCalculation)
-  {
-    timeCalculation -= tempDaysInMonth;
-    tempMonth++;
-    tempDaysInMonth = monthDays[tempMonth - 1];
-  }
-  newEntry->day = timeCalculation;
-  newEntry->month = tempMonth;
-
-  PrintLogEntry(newEntry);
-
-  Serial.print(F("Free Memory:"));
-  int freeMemory = getFreeMemory();
-  Serial.print(String(freeMemory));
-  Serial.println();
-  memoryLimitReached = freeMemory < FreeMemoryLimit;
-  if (memoryLimitReached)
-    Serial.print(F("Memory Limit Reached. Logging stopped. Use command cler to clear loglist!"));
-
-  Serial.println();
-
-  LogEntry *tempEntry = logListStack;
-  logListStack = newEntry;
-  logListStack->next = tempEntry;
-}
-
-void SetBaseTime()
-{
-  byte tempStorage;
-
-  Serial.print(F("month:"));
-  while (!Serial.available())
-  {
-    delay(10);
-  }
-  tempStorage = getUserInput().toInt();
-  if (tempStorage == 0 || tempStorage > 12)
-  {
-    Serial.println();
-    Serial.print(F("invalid month."));
-    return;
-  }
-  baseValues.month = tempStorage;
-
-  Serial.print(F("day:"));
-  while (!Serial.available())
-  {
-    delay(10);
-  }
-  tempStorage = getUserInput().toInt();
-  if (tempStorage == 0 || tempStorage > monthDays[baseValues.month - 1])
-  {
-    Serial.println();
-    Serial.print(F("invalid day. Max Day:"));
-    Serial.print(String(monthDays[baseValues.month - 1]));
-    return;
-  }
-  baseValues.day = tempStorage;
-
-  Serial.print(F("hour:"));
-  while (!Serial.available())
-  {
-    delay(10);
-  }
-  tempStorage = getUserInput().toInt();
-  if (tempStorage > 23)
-  {
-    Serial.println();
-    Serial.print(F("invalid hour. Max Hour:23"));
-    return;
-  }
-  baseValues.hour = tempStorage;
-
-  Serial.print(F("Minute:"));
-  while (!Serial.available())
-  {
-    delay(10);
-  }
-  tempStorage = getUserInput().toInt();
-  if (tempStorage > 59)
-  {
-    Serial.println();
-    Serial.print(F("invalid hour. Max Hour:59"));
-    return;
-  }
-  baseValues.minute = tempStorage;
-  Serial.println();
-  Serial.print(F("new base-time is set."));
-  Serial.println();
-}
-
-void clearLog()
-{
-  Serial.println();
-  memoryLimitReached = false;
-  LogEntry *currentEntry = logListStack;
-  LogEntry *nextEntry;
-  while (currentEntry != NULL)
-  {
-    nextEntry = currentEntry->next;
-    delete currentEntry;
-    currentEntry = NULL;
-    currentEntry = nextEntry;
-  }
-  logListStack = NULL;
-  Serial.print(F("Log cleared."));
-  Serial.println();
-}
